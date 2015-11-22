44e5ef7f44b5a20a8894571ce7f46cf55e1bb4a5
Chih-Feng Lin <phone751230@gmail.com>
2015-11-22 18:16:59 -0500

    add Q1a combiner.py and reducer.py

---
+++ Q1/a/reducer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# The object of this file is to merge the same brand and accumulate the count number
+
+import simplejson as json
+import re
+import sys
+
+
+total_count = 0
+first = True
+for line in sys.stdin:
+	try:
+		brand, count = line.split("\t")
+		if (first):
+			previous_brand = brand
+			first = False
+		
+		total_count += int(count)
+
+		if brand != previous_brand:
+			print previous_brand + "\t" + str(total_count)
+			previous_brand = brand
+			total_count = int(count)
+
+
+	except ValueError as e:
+		continue
+
+# Print the last one
+print previous_brand + "\t" + str(total_count)
